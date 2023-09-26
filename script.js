e68073572cac6ba390757ee07f1bde3de23786a5
Karolina <karolinasoares276@gmail.com>
2023-09-26 09:08:31 -0300

    Adicionando o for com array e of

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ console.log(valor1)
 for (let i=0; i<10; i++){
     console.log("Agora i vale: " + i)
 }
+
+// FOR COM ARRAY
+
+let carros=["fusca", "belina", "marea turbo", "kombi", "gurgel", "monza"]
+
+for(let i=0; i <carros.length; i++){
+    console.log(carros[i])
+}
+
+// for com of
+
+for (let carro of carros){
+    console.log(carro)
+}
